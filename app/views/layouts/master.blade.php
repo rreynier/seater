d0c0508505edd51ba3f01cba62c312203445bfd5
Ryan Barr <ryanbarr@gmail.com>
2014-12-24 14:36:57 -0700

    Front-end seat grid layout and simple styling.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/vendor/bootstrap-3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/seat-selector.css">
         <title>Seater Admin</title>
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -17,5 +19,13 @@
 
     <body>
         @yield('content')
+
+        <script src="/assets/vendor/jquery-1.11.2/jquery.min.js"></script>
+        <script src="/assets/vendor/bootstrap-3.3.1/js/bootstrap.min.js"></script>
+        <script>
+        $(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+        </script>
     </body>
 </html>
--- app/views/seats/index.blade.php
+++ app/views/seats/index.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="seatSection">
     @foreach($rows as $row => $seats)
-        <div class="row-{{ $row }}">
-            <h2>Row {{ $row }}</h2>
-            <ul>
-                @foreach($seats as $seat)
-                    <li class="seat-{{ $seat->number }} row-{{ $row }}-seat-{{ $seat->number }}">
-                        {{ $seat->number }}
-                        @if($seat->user)
-                            claimed by {{ $seat->user->first_name }} {{ $seat->user->last_name }}
-                        @else
-                            unclaimed |
-                            <a href="#">claim with code</a>
-                        @endif
-                    </li>
-                @endforeach
-            </ul>
+        <div class="seatRow seatRow-{{ $row }}">
+            <div class="seatRowTitle">{{ $row }}</div>
+            @foreach($seats as $seat)
+                <div class="seat seat-{{ $seat->number }} row-{{ $row }}-seat-{{ $seat->number }}">
+                    @if($seat->user)
+                        <a href="javascript:;" class="reserved" title="{{ $seat->user->first_name }} {{ $seat->user->last_name }}" data-toggle="tooltip">{{ $row . $seat->number }}</a>
+                    @else
+                        <a href="javascript:;" class="reserveSeat">{{ $row . $seat->number }}</a>
+                    @endif
+                </div>
+            @endforeach
         </div>
     @endforeach
+</div>
 @stop
